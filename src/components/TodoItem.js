b3f52a77893446a4f062092f0fc54da2fc6f2ff2
Claire Cassidy <clacassidy@gmail.com>
2019-05-22 11:31:23 -0700

    created TodoItem.js and added to TodoList.js

---
+++ src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import Component from './Component.js';
+
+class TodoItem extends Component {
+    renderTemplate() {
+        const todo = this.props.todo;
+        let checked = '';
+
+        if(todo.completed) {
+            checked = 'checked';
+        }
+        return /*html*/`
+            <li>
+                <input type="checkbox"${checked}>${todo.task}
+            </li>
+        `;
+    }
+}
+
+export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,20 @@
 import Component from './Component.js';
+import TodoItem from './TodoItem.js';
 
 class TodoList extends Component {
+
+    render() {
+        const list = this.renderDOM();
+
+        const todos = this.props.todos;
+        todos.foreach(todo => {
+            const todoItem = new TodoItem({ todo });
+            const todoItemDOM = todoItem.render();
+
+            list.appendChild(todoItemDOM);
+        });
+        return list;
+    }
     renderTemplate() {
         return /*html*/`
             <ul id="list"></ul>
