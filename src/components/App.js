9dd0877f222b5d7830b5dc3ef00507e5144ab734
Claire Cassidy <clacassidy@gmail.com>
2019-05-24 13:17:48 -0700

    added filterjs and filtertodojs to Appjs

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import Header from './Header.js';
 import TodoList from './TodoList.js';
 import todos from '../data/todos.js';
 import AddTodo from './AddTodo.js';
+import Filter from './Filter.js';
+import filterToDo from '../filter-todos.js';
 
 class App extends Component {
 
     render() {
         const dom = this.renderDOM();
 
+        //Header
         const header = new Header();
         const headerDOM = header.render();
 
+        //Todo List
         const todoList = new TodoList({ 
             todos,
             onRemove:(TodoToRemove) => {
@@ -21,24 +25,35 @@ class App extends Component {
                 todoList.update({ todos });
             }
         });
-
         const todoListDOM = todoList.render();
 
+        //AddToDo
         const addTodo = new AddTodo({
             onAdd: newTodo => {
-                todoList.unshift(newTodo);
+                todos.unshift(newTodo);
                 todoList.update({ todos });
+                filter.update();
             }
         });
         const addTodoDOM = addTodo.render();
 
+        //Filter
+        const filter = new Filter({
+            onFilter: filter => {
+                const filtered = filterToDo(todos, filter);
+                todoList.update({ todos:filtered });
+            }
+        });
+
+        const filterDOM = filter.render();
+
+        //append To Dom
         const main = dom.querySelector('main');
-
+        dom.insertBefore(headerDOM, main);
         main.appendChild(addTodoDOM);
-
+        main.appendChild(filterDOM);
         main.appendChild(todoListDOM);
 
-        dom.insertBefore(headerDOM, main);
 
         return dom;
     }
